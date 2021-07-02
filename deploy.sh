415650259508e556a7df0d3ee386f358baadf64b
Brianna DelValle <bdelvalle@twilio.com>
2021-07-01 19:27:08 -0600

    Update delpoy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,13 @@
 #!/usr/bin/env sh
-
 # abort on errors
 set -e
-
 # build
-npm run build
-
+yarn run build
 # navigate into the build output directory
 cd dist
 
 git init
 git add -A
 git commit -m 'deploy'
-
-
-# if you are deploying to https://bld010.github.io/the-great-debate
-# git push -f git@github.com:bld010/the-great-debate.git main:gh-pages
-
+git push -f git@github.com:bld010/the-great-debate.git main:gh-pages
 cd -
